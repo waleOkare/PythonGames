a362f1eb9fbcbd16edf116b5bef21b824b74e5cc
waleokare <waleokare@outlook.com>
2018-12-04 17:26:56 +0100

    Initial commit

---
+++ RockPaperScissors.py
@@ -0,0 +1,39 @@
+#A Game of Rock, Paper, Scissors played against computer
+from random import randint
+
+player = input('Select rock, paper or scissors (lowercase) ? ')
+
+print('Player pick->>> ', player, 'vs', end=' ')#end= ' ' function tells the computer to end with a space not a new line
+
+chosen = randint(0, 3)
+
+if chosen == 1:
+    computer = 'rock'
+elif chosen == 2:
+    computer = 'paper'
+else:
+    computer = 'scissors'
+
+print(computer, '<<<-Computer Pick')
+
+if player == computer:
+    print('ITS A DRAW!!!')
+elif player == 'rock' and computer == 'scissors':
+    print('YOU WIN!!!')
+elif player == 'paper' and computer == 'scissors':
+    print('COMPUTER WINS!!!')
+elif player == 'scissors' and computer == 'rock':
+    print('COMPUTER WINS!!!')
+elif player == 'scissors' and computer == 'paper':
+    print('YOU WIN!!!')
+elif player == 'paper' and computer == 'rock':
+    print('YOU WIN!!!')
+elif player == 'rock' and computer == 'paper':
+    print('COMPUTER WINS!!!')
+
+
+
+
+
+
+
